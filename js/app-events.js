d847f4e40fccc1da997d2cde6f33f661f46bd455
shilohg25 <shiloh.gelito@gmail.com>
2026-04-17 14:42:00 -0700

    Update app-events.js

--- js/app-events.js
+++ js/app-events.js
@@ -1,1 +1,70 @@
+(function () {
+  "use strict";
 
+  function initAppEvents(deps) {
+    const {
+      el,
+      login,
+      openChangePasswordModal,
+      runWithBusyState,
+      saveOwnPassword,
+      closeModal,
+      logout,
+      setView,
+      openAddCustomerModal,
+      renderCustomerList,
+      renderCustomerDiscountFields,
+      addContactRow,
+      saveCustomer,
+      openInvoiceModalForCreate,
+      renderInvoiceDiscountControls,
+      updateInvoiceTotal,
+      addLineItemRow,
+      saveInvoice,
+      openPaymentTypeModal,
+      openPayByInvoiceStep,
+      openPartialPaymentStep,
+      proceedSelectedInvoices,
+      renderPartialBalanceInfo,
+      proceedPartialPayment,
+      renderPaymentMethodFields,
+      renderPaymentReviewBox,
+      renderWithholdingTaxUi,
+      savePayment,
+      renderExecutiveView,
+      renderReportsView,
+      clearReportFilters,
+      downloadReportCsv,
+      printReport,
+      renderPaymentReceivedReportView,
+      clearPaymentReportFilters,
+      downloadPaymentReportCsv,
+      printPaymentReceivedReport,
+      renderTbvRequestsTable,
+      downloadTbvReportCsv,
+      printTbvReport,
+      saveTbvRequest,
+      decideTbv,
+      openSoaModal,
+      renderSoaPaymentRangeVisibility,
+      autofillSoaPaymentRange,
+      generateSoa,
+      openEditCustomerModal,
+      deleteSelectedCustomer,
+      renderAccountsView,
+      refreshAccounts,
+      openCreateAccountModal,
+      saveAccount,
+      saveResetPassword,
+      toggleLogSort
+    } = deps || {};
+
+    // Paste the exact deleted bindEvents() function block here, unchanged.
+
+    return { bindEvents };
+  }
+
+  window.AKY_APP_EVENTS = Object.freeze({
+    initAppEvents
+  });
+})();
