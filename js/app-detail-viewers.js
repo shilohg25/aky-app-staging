40589d611cc4eb0425eb003b9482535e27dbf7b9
shilohg25 <shiloh.gelito@gmail.com>
2026-04-17 15:03:43 -0700

    Update app-detail-viewers.js

--- js/app-detail-viewers.js
+++ js/app-detail-viewers.js
@@ -1,1 +1,41 @@
+(function () {
+  "use strict";
 
+  function initAppDetailViewers(deps) {
+    const {
+      supabaseClient,
+      state,
+      el,
+      getSelectedCustomer,
+      canEditInvoiceRecord,
+      canRequestTbv,
+      openInvoiceModalForEdit,
+      openTbvModal,
+      openModal,
+      closeModal,
+      getPaymentDetailsObject,
+      getChequeStatus,
+      getInvoiceReferenceLabel,
+      getPaymentTypeLabel,
+      getPaymentCollectionReceipt,
+      getAllocationAmount,
+      formatPeso,
+      formatNumber,
+      formatDateTime,
+      escapeHtml
+    } = deps || {};
+
+    // Paste these exact deleted function blocks here, unchanged, in this order:
+    // 1. viewInvoice
+    // 2. viewPayment
+
+    return {
+      viewInvoice,
+      viewPayment
+    };
+  }
+
+  window.AKY_APP_DETAIL_VIEWERS = Object.freeze({
+    initAppDetailViewers
+  });
+})();
