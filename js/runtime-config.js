742e4b5c3ce508f249d4be4f9441c0ab0f265100
shilohg25 <shiloh.gelito@gmail.com>
2026-04-18 13:05:29 -0700

    Create runtime-config.js

---
+++ js/runtime-config.js
@@ -0,0 +1,6 @@
+window.APP_CONFIG = {
+  supabaseUrl: "https://hghssblwbezmyrrfthri.supabase.co",
+  supabaseAnonKey: "sb_publishable_IL6vSMDa-G1kpWCMR0KKXA_9NtUYIMr",
+  edgeFunctionBaseUrl: "https://hghssblwbezmyrrfthri.supabase.co/functions/v1",
+  appEnv: "staging"
+};
